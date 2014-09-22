ba3c27fbf3d5c88c7870b536bac1fd6d3b514fcc
Daniele Gallingani <daniele@buildo.io>
2014-09-22 10:46:21 +0200

    [core] xls cleaning and date parsing

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var assert = require('assert');
 XLS = require('xlsjs');
 var workbook = XLS.readFile('data/buildo.xls');
 var sheet = workbook.Sheets['Contabilità'];
@@ -13,5 +14,24 @@ function parseByLine(sheet) {
       data[parts[1]] = {};
     data[parts[1]][parts[0]] = cell.v;
   }
+  var data = data.filter(function(a) {
+    var firstColumn = a.A;
+    return  firstColumn == 'Fatture ricevute' ||
+            firstColumn == 'Fatture emesse' ||
+            parseInt(firstColumn) === firstColumn;
+
+  }).map(parseXLSDate);
   return data;
+}
+
+
+function parseXLSDate(a) {
+  var b = a.B;
+  if (!b)
+    return a;
+  var date = b.split('/');
+  assert(date.length == 3, 'invalid date format');
+  b = new Date(date[2], date[1], date[0]);
+  a.B = b;
+  return a;
 }
