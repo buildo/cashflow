53915cce83a24c7d55b66c44056a2da4d678f683
Francesco <firefelix@gmail.com>
2014-10-19 17:10:40 +0200

    [core] defined test for implicitValues module

--- src/tests/tests.js
+++ src/tests/tests.js
@@ -175,4 +175,33 @@ describe('insertDefaultValues', () => {
   });
 });
 
+describe('insertImplicitValues', () => {
+  it('should create property uncertainty set to 0 inside expctedAmount', () => {
+    const mergedCFF = [
+      {
+        sourceId: 'MERGE_MODULE',
+        sourceDescription: 'desc1',
+        priority: 5,
+        lines: [
+          {
+            id: 'uniqueID',
+            amount: {
+              vat: 3,
+              gross: 15
+            },
+          }
+        ]
+      }
+    ]; 
+    const x = processInputs(mergedCFF).toJS().lines;
+    expect(Array.isArray(x)).to.be.true;
+    expect(x).to.have.length(1);
+    expect(x[0]).to.have.property('amount');
+    expect(x[0].amount).to.have.property('net', 12);
+    expect(x[0].amount).to.have.property('gross', 15);
+    expect(x[0].amount).to.have.property('vat', 3);
+    expect(x[0].amount).to.have.property('vatPercentage', 0.2);
+  });
+});
+
 /*jshint ignore:end*/
