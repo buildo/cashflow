8efe45f5678caf6f66a6bcafad10b42d9f7a10a4
Francesco <firefelix@gmail.com>
2014-11-03 19:07:27 +0100

    [core] creditCardsReport: modified outputreturns array filled with an object for each month;each monthObject contains infos for that month (year, month, monthNumber, totalAmount) and an array 'people' with infos for each person (totalAmount, name, single payments)

--- src/reports/credit_cards/creditCardsReport.js
+++ src/reports/credit_cards/creditCardsReport.js
@@ -1,38 +1,65 @@
 'use strict';
 
 const Immutable = require('immutable');
+const getFilteredPayments = require('./creditCardsFilters.js');
 
-const generateCreditCardsReport = (cff) => {
+const generateCreditCardsReport = (cff, configs) => {
 
-  const getVerbalMonth = (monthIndex) => {
+  const getVerbalMonth = (monthNumber) => {
     const months = ['gennaio', 'febbraio', 'marzo', 'aprile', 'maggio', 'giugno', 'luglio', 'agosto', 'settembre', 'ottobre', 'novembre', 'dicembre'];
-    return months[monthIndex - 1];
+    return months[monthNumber - 1];
   };
 
   const groupedPayments = cff.get('lines').flatMap((line) => line.get('payments'));
-  const filteredPayments = groupedPayments.filter((payment) => payment.has('date') && payment.has('grossAmount') &&
-    payment.has('method') && payment.get('methodType') === 'credit card');
+  const filteredPayments = getFilteredPayments(groupedPayments, configs.filterParameters);
 
-  const updater = (immutableObj, payment) => {
-    immutableObj = immutableObj.set('cumulativeAmount', immutableObj.get('cumulativeAmount') + payment.get('grossAmount'));
-    const paymentInfo = Immutable.Map({
-      amount: payment.get('grossAmount'),
-      date: payment.get('date')
-    });
-    immutableObj = immutableObj.set('payments', immutableObj.get('payments').push(paymentInfo));
-    return immutableObj;
+  const updateMonth = (monthMap, payment) => {
+    const person = payment.get('method').split(' ')[1];
+
+    const updatePerson = (personMap) => {
+      personMap = personMap.set('totalAmount', personMap.get('totalAmount') + payment.get('grossAmount'));
+      const paymentInfo = Immutable.Map({
+        amount: payment.get('grossAmount'),
+        date: payment.get('date')
+      });
+      personMap = personMap.set('payments', personMap.get('payments').push(paymentInfo));
+      return personMap;
+    };
+
+    const emptyPerson = Immutable.fromJS(
+      {
+        name: payment.get('method').split(' ')[1],
+        totalAmount: 0,
+        payments: []
+      }
+    );
+
+    monthMap = monthMap.set('totalAmount', monthMap.get('totalAmount') + payment.get('grossAmount'));
+    monthMap = monthMap.updateIn(['people', person], emptyPerson, updatePerson);
+    return monthMap;
   };
 
   const reportWithKeys = filteredPayments.reduce((acc, payment) => {
-      const verbalMonth = getVerbalMonth(parseInt(payment.get('date').split('-')[1]));
-      const person = payment.get('method').split(' ')[1];
-      const emptyObj = Immutable.fromJS({person: person, cumulativeAmount: 0, payments: []});
-      return acc.updateIn([verbalMonth, person], emptyObj, (immutableObj) => updater(immutableObj, payment));
+      const dateArray = payment.get('date').split('-');
+      const shouldBeInFollowingMonth = dateArray[2] >= 29;
+      const monthNumber = parseInt(dateArray[1]) + (shouldBeInFollowingMonth ? 1 : 0);
+      const emptyMonth = Immutable.fromJS(
+        {
+          year: dateArray[0],
+          monthNumber: monthNumber,
+          month: getVerbalMonth(monthNumber),
+          totalAmount: 0,
+          people: {}
+        }
+      );
+      return acc.update(getVerbalMonth(monthNumber), emptyMonth, (monthMap) => updateMonth(monthMap, payment));
     },
     Immutable.Map()
   );
 
-  return Immutable.Map({creditCards: reportWithKeys.map((monthReport) => monthReport.toVector()).toMap()});
+  const report = reportWithKeys.map((monthReport) => monthReport.set('people', monthReport.get('people').toVector())).toVector();
+
+  return Immutable.Map({creditCards: report.sort((a, b) => a.get('monthNumber') > b.get('monthNumber'))});
 };
 
 module.exports = generateCreditCardsReport;
