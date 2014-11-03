86c6fbb59a84a46e1f2ff4e6e839fe140bd0f3dc
Francesco <firefelix@gmail.com>
2014-11-03 16:48:55 +0100

    [core] creditCardsReport: implemented correct credit cards reportreturns object with informations for each credit card (person) payments grouped for month.informations are: person; cumulativeAmount (spent); payments (array with info for each single payment).

--- src/reports/credit_cards/creditCardsReport.js
+++ src/reports/credit_cards/creditCardsReport.js
@@ -3,9 +3,36 @@
 const Immutable = require('immutable');
 
 const generateCreditCardsReport = (cff) => {
+
+  const getVerbalMonth = (monthIndex) => {
+    const months = ['gennaio', 'febbraio', 'marzo', 'aprile', 'maggio', 'giugno', 'luglio', 'agosto', 'settembre', 'ottobre', 'novembre', 'dicembre'];
+    return months[monthIndex - 1];
+  };
+
   const groupedPayments = cff.get('lines').flatMap((line) => line.get('payments'));
-  const totalAmount = groupedPayments.reduce((acc, payment) => payment.methodType === 'credit card' ? acc + payment.grossAmount : acc);
-  return Immutable.Map({creditCards: totalAmount});
+  const filteredPayments = groupedPayments.filter((payment) => payment.has('date') && payment.has('grossAmount') &&
+    payment.has('method') && payment.get('methodType') === 'credit card');
+
+  const updater = (immutableObj, payment) => {
+    immutableObj = immutableObj.set('cumulativeAmount', immutableObj.get('cumulativeAmount') + payment.get('grossAmount'));
+    const paymentInfo = Immutable.Map({
+      amount: payment.get('grossAmount'),
+      date: payment.get('date')
+    });
+    immutableObj = immutableObj.set('payments', immutableObj.get('payments').push(paymentInfo));
+    return immutableObj;
+  };
+
+  const reportWithKeys = filteredPayments.reduce((acc, payment) => {
+      const verbalMonth = getVerbalMonth(parseInt(payment.get('date').split('-')[1]));
+      const person = payment.get('method').split(' ')[1];
+      const emptyObj = Immutable.fromJS({person: person, cumulativeAmount: 0, payments: []});
+      return acc.updateIn([verbalMonth, person], emptyObj, (immutableObj) => updater(immutableObj, payment));
+    },
+    Immutable.Map()
+  );
+
+  return Immutable.Map({creditCards: reportWithKeys.map((monthReport) => monthReport.toVector()).toMap()});
 };
 
 module.exports = generateCreditCardsReport;
