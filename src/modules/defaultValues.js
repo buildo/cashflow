e74d1317d53536fa10f2aab85e11542c545f73dd
Francesco <firefelix@gmail.com>
2014-11-04 11:43:48 +0100

    [core] inserDefaultValues: defined field currency inside each linein case line is missing currency or have empty object it is set as default to euros values.

--- src/modules/defaultValues.js
+++ src/modules/defaultValues.js
@@ -3,12 +3,12 @@
 const Immutable = require('immutable');
 
 const getLinesWithDefaultValues = (lines) => {
-  return lines.reduce((acc, line) => {
-      const enabled = line.has('enabled') ? line.get('enabled') : true;
-      return acc.push(line.set('enabled', enabled));
-    },
-    Immutable.Vector()
-  );
+  return lines.map((line) => {
+    line = line.set('enabled', line.has('enabled') ? line.get('enabled') : true);
+    line = line.has('currency') && (line.get('currency').has('conversion') || line.get('currency').has('name')) ? line
+      : line.set('currency', Immutable.Map({name: 'EUR', conversion: 1}));
+    return line;
+  }).toVector();
 };
 
 const insertDefaultValues = (cff) => {
--- src/modules/defaultValues.spec.js
+++ src/modules/defaultValues.spec.js
@@ -24,6 +24,43 @@ const mergedCFF = {
         gross: 15,
         vatPercentage: 0.2
       }
+    },
+    {
+      id: 'LINE_ID2',
+      mergedFrom: ['first','second'],
+      currency: {},
+      amount: {
+        net: 12,
+        vat: 3,
+        gross: 15,
+        vatPercentage: 0.2
+      }
+    },
+    {
+      id: 'LINE_ID3',
+      mergedFrom: ['first','second'],
+      currency: {
+        name: 'USD'
+      },
+      amount: {
+        net: 12,
+        vat: 3,
+        gross: 15,
+        vatPercentage: 0.2
+      }
+    },
+    {
+      id: 'LINE_ID4',
+      mergedFrom: ['first','second'],
+      currency: {
+        conversion: 1.23
+      },
+      amount: {
+        net: 12,
+        vat: 3,
+        gross: 15,
+        vatPercentage: 0.2
+      }
     }
   ]
 };
@@ -36,7 +73,20 @@ const lines = output.lines;
 describe('insertDefaultValues', () => {
   it('should create property enabled set to true', () => {
     expect(Array.isArray(lines)).to.be.true;
-    expect(lines).to.have.length(1);
+    expect(lines).to.have.length(mergedCFF.lines.length);
     expect(lines[0]).to.have.property('enabled', true);
   });
+  it('should create field currency or fill empty ones with name and conversion set to euros', () => {
+    expect(lines[0].currency).to.have.property('name', 'EUR');
+    expect(lines[0].currency).to.have.property('conversion', 1);
+    expect(lines[1].currency).to.have.property('name', 'EUR');
+    expect(lines[1].currency).to.have.property('conversion', 1);
+  });
+  it('should leave incomplete field currency untouched', () => {
+    expect(lines[2].currency).to.have.property('name', 'USD');
+    expect(lines[2].currency).not.to.have.property('conversion');
+    expect(lines[3].currency).not.to.have.property('name');
+    expect(lines[3].currency).to.have.property('conversion', 1.23);
+  });
+
 });
