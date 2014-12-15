5f0c20cadc0336d1509fecc3f22d2ce04f8134d8
Francesco <firefelix@gmail.com>
2014-12-15 13:13:28 +0100

    [web] moved initial actions from getInitialState to componentDidMount

--- src/js/components/analytics/AnalyticsMain.jsx
+++ src/js/components/analytics/AnalyticsMain.jsx
@@ -5,25 +5,43 @@
 const React = require('react');
 const RouteHandler = require('react-router').RouteHandler;
 const ServerActions = require('../../actions/ServerActions');
+const CFFStore = require('../../store/CFFStore.js');
+
+const getStateFromStores = function () {
+  return {
+    isLoadingMainCFF: CFFStore.isLoading(),
+  };
+};
 
 const AnalyticsMain = React.createClass({
 
   getInitialState: function() {
-    ServerActions.updateMain();
-    return {};
+    return getStateFromStores();
   },
 
   componentDidMount: function() {
-    // ServerActions.updateMain();
+    CFFStore.addChangeListener(this._onChange);
+    if (!CFFStore.getMainCFF()) {
+      ServerActions.updateMain();
+    }
+  },
+
+  componentWillUnmount: function() {
+    CFFStore.removeChangeListener(this._onChange);
   },
 
   render: function () {
+    console.log('RENDER_ANALYTICS');
     return (
       <div>
-        <RouteHandler/>
+        <RouteHandler isLoadingMainCFF={this.state.isLoadingMainCFF}/>
       </div>
     );
   },
+
+  _onChange: function() {
+    this.setState(getStateFromStores());
+  }
 
 });
 
