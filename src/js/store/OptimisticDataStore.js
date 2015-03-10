44675b01e862cef1076d6a16667e5244e24efd07
Francesco <firefelix@gmail.com>
2015-03-10 19:20:41 +0100

    [web] refactored OptimisticDataStore for Alt integrationmodified OptimisticDataStore to work with classes

--- src/js/store/OptimisticDataStore.js
+++ src/js/store/OptimisticDataStore.js
@@ -4,71 +4,100 @@ const Immutable = require('immutable');
 const _ = require('lodash');
 const DataStore = require('./DataStore');
 
-// TODO: clean up optimisitc values on stable inserts?
-const getterMethods = ['get', 'getAll'];
 const setterMethods = ['upsert', 'insert', 'update', 'delete', 'deleteAll'];
 
-const camelCase = function(prefix, name) {
-  return prefix + name.charAt(0).toUpperCase() + name.substring(1);
+function NoopClass() {}
+const excluded = Object.getOwnPropertyNames(NoopClass.prototype);
+
+const buildClassFunctions = (classObj, context) => {
+  const obj = classObj.prototype;
+  const self = context;
+  Object.getOwnPropertyNames(obj).forEach(function (functionName) {
+    if (excluded.indexOf(functionName) === -1) {
+      // create optimistic/fail/success functions
+      self[functionName + 'Optimistic'] = (payload) => {
+        console.log('OPTMISTIC');
+        self[functionName](_.extend(payload, {__optimistic: true, __undo: false}));
+      };
+      self[functionName + 'Success'] = (payload) => {
+        console.log('SUCCESS');
+        self[functionName](_.extend(payload, {__optimistic: false, __undo: false}));
+      };
+      self[functionName + 'Undo'] = (payload) => {
+        self[functionName](_.extend(payload, {__optimistic: false, __undo: true}));
+      };
+    }
+  });
 };
 
-const omitOptimistic = function(o) {
-  return _.isObject(o) ? _.omit(o, '__optimistic') : o;
-};
-
-const self = _.extend({}, Object.keys(DataStore).reduce((self, prop) => {
-  if (getterMethods.indexOf(prop) !== -1) {
-    self[prop] = function() {
-      let stableResult = DataStore[prop].apply(this, arguments);
-      let optStore = _.extend({}, this, { _data: this._optimisticData });
-      let optResult = DataStore[prop].apply(optStore, arguments);
-      if (prop === 'getAll') {
-        // TODO: better policy
-        return _.uniq(stableResult.concat(optResult), '_id');
-      } else {
-        let stableResultImm = Immutable.fromJS(stableResult || {});
-        let resultImm = stableResultImm.mergeDeep(Immutable.fromJS(optResult));
-        let result = resultImm.toJS();
-        // TODO: better policy
-        return _.extend(result, {
-          __optimistic: !Immutable.is(resultImm, stableResultImm)
-        });
-      }
-    };
-  } else if (setterMethods.indexOf(prop) !== -1) {
-    self[prop] = function() {
-      let selfData = this._data;
-      let optimistic = this.isOptimisticHandling();
-      if (optimistic) {
-        this._data = this._optimisticData;
-      }
-      let args = Array.prototype.slice.call(arguments, 0);
-      let ret = DataStore[prop].apply(this, args.map(omitOptimistic));
-      if (optimistic) {
-        this._optimisticData = this._data;
-        this._data = selfData;
-      }
-      return ret;
-    };
-    self[camelCase('optimistic', prop)] = function() {
-      let prevHandling = this.isOptimisticHandling();
-      this.setOptimisticHandling(true);
-      let ret = self[prop].apply(this, arguments);
-      this.setOptimisticHandling(prevHandling);
-      return ret;
-    };
-    self[camelCase('stable', prop)] = function() {
-      let prevHandling = this.isOptimisticHandling();
-      this.setOptimisticHandling(false);
-      let ret = self[prop].apply(this, arguments);
-      this.setOptimisticHandling(prevHandling);
-      return ret;
-    };
+const handleUndo = (id, context) => {
+  const self = context;
+  if (!self._optimisticData.has(id) || (self._data.has(id) && self._optimisticData.has(id))) {
+    self._optimisticData = self._optimisticData.set(id, self._data.get(id));
+  } else {
+    self._optimisticData = self._optimisticData.delete(id);
   }
-  return self;
-}, {}));
+};
 
-module.exports = _.extend(self, {
-  _data: Immutable.Map({}),
-  _optimisticData: Immutable.Map({})
-});
+const handleSuccess = (id, context) => {
+  const self = context;
+  if (self._optimisticData.has(id)) {
+    self._data = self._data.set(id, self._optimisticData.get(id));
+  } else {
+    self._data = self._data.delete(id);
+  }
+};
+
+
+class OptimisticDataStore extends DataStore {
+
+  constructor(classObj) {
+    this._optimisticData = Immutable.Map();
+    this._data = Immutable.Map();
+    classObj.getAll = OptimisticDataStore.getAll;
+    classObj.get = OptimisticDataStore.get;
+    buildClassFunctions(classObj, this);
+
+    const self = this;
+    setterMethods.forEach((m) => {
+      self[m] = (id, data) => {
+        let selfData;
+        if (!data || typeof data.__optimistic === 'undefined') {
+          // stable method
+          super[m](id, data);
+          selfData = self._data;
+          self._data = self._optimisticData;
+          super[m](id, data);
+          self._optimisticData = self._data;
+          self._data = selfData;
+        }
+        else if (data.__undo) {
+          handleUndo(id, self);
+        } else if (data.__optimistic) {
+          selfData = self._data;
+          self._data = self._optimisticData;
+          super[m](id, data);
+          self._optimisticData = self._data;
+          self._data = selfData;
+          // console.log('OPTIMISTIC', self._optimisticData.toJS(), self._data.toJS());
+        } else {
+          handleSuccess(id, self);
+        }
+      };
+    });
+  }
+
+  static get(id, missingValue) {
+    const value = this.getState()._optimisticData.get(id + '', missingValue);
+    return value && value.toJS ? value.toJS() : value;
+  }
+
+  static getAll() {
+    return Object.keys(this.getState()._optimisticData.toJS()).map(id => {
+      return this.get(id);
+    });
+  }
+
+}
+
+module.exports = OptimisticDataStore;
